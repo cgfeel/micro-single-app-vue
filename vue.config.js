d2d50c751151b00d4538827b0a7ba5fdf95e81f6
levi <cgfeel@gmail.com>
2026-07-13 01:45:53 +0800

    fix: update vue add import-map

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const { CustomStandaloneDisabledPlugin, GenerateImportMapPlugin } = require("@event-chat/micro-dev-config/plugins")
 
-// 自定义 standalone disabled 提示页内容
-class CustomStandaloneDisabledPlugin {
-  apply(compiler) {
-    const isProd = compiler.options.mode === 'production'
-    compiler.hooks.compilation.tap('CustomStandaloneDisabledPlugin', (compilation) => {
-      const HtmlWebpackPlugin = require('html-webpack-plugin')
-      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
-        'CustomStandaloneDisabledPlugin',
-        (data, cb) => {
-          // 仅当 standalone plugin 处于 disabled 状态时，替换默认提示页
-          if (data.html.includes('Your Microfrontend is not here')) {
-            data.html = data.html.replace(
-              /<main>[\s\S]*<\/main>/,
-              `<main>
-                <h1>Single-spa: Vue 子应用</h1>
-                <p>当前微前端以 "集成模式" 运行，不独立提供页面。</p>
-                <h2>访问链接</h2>
-                <p>${isProd ? '<a href="/micro-single-app-substrate/vue/">/micro-single-app-substrate/vue/</a>' : '<a href="http://localhost:9000/vue">http://localhost:9000/vue</a>'}</p>
-                <h2>Standalone 模式</h2>
-                <p>如果想独立运行本子应用：<code>npm run serve:standalone</code></p>
-              </main>`
-            )
-          }
-          cb(null, data)
-        }
-      )
-    })
-  }
-}
+const isProduction = process.env.NODE_ENV === 'production';
+
+const displayStandalonePage = (prod) => `<main>
+  <h1>Single-spa: React 子应用</h1>
+  <p>当前微前端以"集成模式"运行，不独立提供页面。</p>
+  <h2>访问链接</h2>
+  <p>${
+    prod
+      ? '<a href="/micro-single-app-substrate/react/">/micro-single-app-substrate/react/</a>'
+      : '<a href="http://localhost:9000/react">http://localhost:9000/react</a>'
+  }</p>
+  <h2>Standalone 模式</h2>
+  <p>如果想独立运行本子应用：<code>npm run start:standalone</code></p>
+</main>`
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -50,6 +36,16 @@ module.exports = defineConfig({
     }
   },
   configureWebpack: {
-    plugins: [new CustomStandaloneDisabledPlugin()]
+    plugins: [
+      new CustomStandaloneDisabledPlugin({
+        handle: (html) => !html.includes('Your Microfrontend is not here') ? html : html.replace(
+          /<main>[\s\S]*<\/main>/,
+          displayStandalonePage(isProduction)
+        )
+      }),
+      new GenerateImportMapPlugin({
+        module: isProduction ? '@levi/vue-project' : ''
+      })
+    ]
   }
 })
