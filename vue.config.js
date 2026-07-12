318bcdea76f6ecff3c4601f2531aaf3d99d855ac
levi <cgfeel@gmail.com>
2026-07-12 16:27:10 +0800

    fix: update config

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,15 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "http://localhost:4000",
   devServer: {
-    port: 4000
+    port: 4000,
+    client: {
+      webSocketURL: {
+        protocol: "ws",
+        hostname: "localhost",
+        port: 4000,
+        pathname: "/ws",
+      },
+    },
   },
   chainWebpack: (config) => {
     if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) {
